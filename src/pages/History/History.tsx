d1be57803362e6bee7102007df93f8fef76f4737
Felipe Guarnieri <guarnieri007@hotmail.com>
2024-07-04 20:15:43 -0300

    Creating the status circle
    Added the status circle with color on the history

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./HistoryStyles";
+import { HistoryContainer, HistoryList, Status } from "./HistoryStyles";
 
 export function History() {
     return (
@@ -19,25 +19,25 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 3 meses</td>
-                            <td>Concluído</td>
+                            <td><Status statusColor={"green"}>Concluído</Status></td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 3 meses</td>
-                            <td>Concluído</td>
+                            <td><Status statusColor={"yellow"}>Concluído</Status></td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 3 meses</td>
-                            <td>Concluído</td>
+                            <td><Status statusColor={"red"}>Concluído</Status></td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 3 meses</td>
-                            <td>Concluído</td>
+                            <td><Status statusColor={"yellow"}>Concluído</Status></td>
                         </tr>
                     </tbody>
                 </table>
